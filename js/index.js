e50e292d56d78cef6d9e9b1a3d0c91cade230c58
Samuel Adama-Doh <samueladamadoh@gmail.com>
2020-01-16 16:00:22 +0000

    edited

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ class TypeWriter {
     }
 
     // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">S${this.txt}</span>`;
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     // Initial Type Speed
     let typeSpeed = 300;
